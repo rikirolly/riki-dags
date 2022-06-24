bc3d8a938741952e4ab7e31780bda4e0f861b658
Riccardo Rolando <riccardo.rolando@roj.com>
2022-06-24 10:55:22 +0200

    tag riki

--- riki_etl_dag.py
+++ riki_etl_dag.py
@@ -48,7 +48,7 @@ with DAG(
     schedule_interval="@daily",
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
-    tags=['example'],
+    tags=['riki'],
 ) as dag:
     # [END instantiate_dag]
     # [START documentation]
