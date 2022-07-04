b7be3944b1b152ccae94d063fbad044906960e0f
Riccardo Rolando <riccardo.rolando@gmail.com>
2022-07-05 00:18:15 +0200

    fix

--- slack_dag.py
+++ slack_dag.py
@@ -13,28 +13,28 @@ with DAG(
     tags=['riki'],
 ) as dag:
 
-SLACK_CONN_ID = 'Slack'
+    SLACK_CONN_ID = 'Slack'
 
-slack_webhook_token = BaseHook.get_connection(SLACK_CONN_ID).password
-slack_msg = """
-            :red_circle: Task Failed. 
-            *Task*: {task}  
-            *Dag*: {dag} 
-            *Execution Time*: {exec_date}  
-            *Log Url*: {log_url} 
-            """.format(
-    task=context.get('task_instance').task_id,
-    dag=context.get('task_instance').dag_id,
-    ti=context.get('task_instance'),
-    exec_date=context.get('execution_date'),
-    log_url=context.get('task_instance').log_url,
-)
-failed_alert = SlackWebhookOperator(
-    task_id='slack_test',
-    http_conn_id='Slack',
-    webhook_token=slack_webhook_token,
-    message=slack_msg,
-    username='airflow',
-    dag=dag)
+    slack_webhook_token = BaseHook.get_connection(SLACK_CONN_ID).password
+    slack_msg = """
+                :red_circle: Task Failed. 
+                *Task*: {task}  
+                *Dag*: {dag} 
+                *Execution Time*: {exec_date}  
+                *Log Url*: {log_url} 
+                """.format(
+        task=context.get('task_instance').task_id,
+        dag=context.get('task_instance').dag_id,
+        ti=context.get('task_instance'),
+        exec_date=context.get('execution_date'),
+        log_url=context.get('task_instance').log_url,
+    )
+    failed_alert = SlackWebhookOperator(
+        task_id='slack_test',
+        http_conn_id='Slack',
+        webhook_token=slack_webhook_token,
+        message=slack_msg,
+        username='airflow',
+        dag=dag)
 
-failed_alert
+    failed_alert
